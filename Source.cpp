4ded9eec7e39473f0ce2a3d176d19bdef092c6d0
Endi Haxhi <endi.haxhi@yahoo.com>
2016-09-02 16:06:35 +0200

    UpToDate

--- Source.cpp
+++ Source.cpp
@@ -5,32 +5,117 @@
 #include <ctime>
 #include <cassert>
 #include <fstream>
+#include <cstring>
+
+namespace ArrayOps
+{
+	void initArray(int arr[], int aSize);
+	void printArray(const int arr[], int aSize);
+	void fillArray(int arr[], int aSize);
+	int linearSearch(const int arr[], int aSize, int nrToSearch);
+	void bubbleSort(int arr[], int aSize);
+	void selectionSort(int arr[], int aSize);
+}
 
 using namespace std;
-
-double larger(double x, double y);
-double maximum(double x, double y, double z);
+using namespace ArrayOps;
 
 int main()
 {
-	double num1, num2, num3;
+	const int aSize = 10;
+	int arr[aSize];
 
-	cout << "Enter 3 numbers: " << endl;
-	cin >> num1 >> num2 >> num3;
+	initArray(arr, aSize);
 
-	cout << maximum(num1, num2, num3) << " is the biggest number." << endl;
+	fillArray(arr, aSize);
 
+	cout << "Unsorted array: " << endl;
+	printArray(arr, aSize);
+	
+	selectionSort(arr, aSize);
+
+	cout << "Sorted array: " << endl;
+	printArray(arr, aSize);
+	
 	system("pause");
 }
-double larger(double x, double y)
+
+void ArrayOps::initArray(int arr[], int aSize)
 {
-	if(x > y)
-		return x;
-	else
-		return y;
+	for(int i = 0; i < aSize; i++)
+	{
+		arr[i] = 0;
+	}
 }
 
-double maximum(double x, double y, double z)
+void ArrayOps::printArray(const int arr[], int aSize)
 {
-	return larger( z, larger(x, y) );
+	for(int i = 0; i < aSize; i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
+void ArrayOps::fillArray(int arr[], int aSize)
+{
+	for(int i = 0; i < aSize; i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+int ArrayOps::linearSearch(const int arr[], int aSize, int nrToSearch)
+{
+	int position;
+
+	for(position = 0; position < aSize; position++)
+	{
+		if(arr[position] == nrToSearch)
+		{
+			return position;
+		}
+	}
+
+	return -1;
+}
+
+void ArrayOps::bubbleSort(int arr[], int aSize)
+{
+	int temp;
+
+	for(int i = 1; i < aSize; i++)
+	{
+		for(int j = 0; j < aSize - i; j++)
+		{
+			if(arr[j] > arr[j + 1])
+			{
+				temp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+}
+
+void ArrayOps::selectionSort(int arr[], int aSize)
+{
+	int temp;
+	int smallestIndex;
+
+	for(int i = 0; i < aSize; i++)
+	{
+		smallestIndex = i;
+
+		for(int j = i + 1; j < aSize; j++)
+		{
+			if(arr[j] < arr[smallestIndex])
+			{
+				smallestIndex = j;
+			}
+		}
+
+		temp = arr[i];
+		arr[i] = arr[smallestIndex];
+		arr[smallestIndex] = temp;
+	}
 }
